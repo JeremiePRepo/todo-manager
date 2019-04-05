9f713f205210650805df5b53d17d39f2bc2f7bb5
Jérémie Pasquis <jeremie_DWWM@cefimmac13.local>
2019-04-05 17:25:52 +0200

    Task form WIP

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -2,13 +2,16 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Task;
+use App\Form\TaskType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PagesController extends AbstractController
 {
     /**
-     * @Route("/", name="pages")
+     * @Route("/", name="index")
      */
     public function index()
     {
@@ -23,5 +26,29 @@ class PagesController extends AbstractController
     public function todolist()
     {
         return $this->render('pages/todolist.html.twig');
+    }
+
+    /**
+     * @Route("/todolist/create", name="create_task")
+     */
+    public function create(Request $request)
+    {
+        // Prépare la création d'un nouvel article
+        $task = new Task();
+        $form = $this->createForm(TaskType::class, $task);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Si le formulaire a été envoyé
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($task);
+            $manager->flush();
+            $this->addFlash('info', 'La tâche a bien été ajouté');
+
+            return $this->redirectToRoute('todolist');
+        }
+
+        return $this->render('pages/tasks/create.html.twig', array('taskForm' => $form->createView()));
     }
 }
